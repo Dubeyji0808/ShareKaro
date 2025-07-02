e65533cc6e2f9347e71c9fed27490968b34b64fc
Ayush <ayushdubey12131213@gmail.com>
2025-07-02 17:08:26 +0530

    fixed all error

--- sharekaro/src/main/java/ayush/Controller/FileController.java
+++ sharekaro/src/main/java/ayush/Controller/FileController.java
@@ -1,4 +1,4 @@
-package src.main.java.ayush.Controller;
+package ayush.Controller;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
@@ -14,6 +14,7 @@ import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.apache.commons.io.IOUtils;
+
 
 public class FileController {
 
